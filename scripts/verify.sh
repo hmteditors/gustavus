d5add4c6ab4a3ada2b781875c385255599d1b0c1
Stephanie Lindeborg <smlind13@g.holycross.edu>
2014-10-17 18:33:35 -0400

    updated verify script and inventory so that full verification will work in this repo

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -5,7 +5,7 @@ if [ "$#" -ne 1 ]; then
 fi
 
 
-echo "Verifying from configuration in ${DIR}configs/vm-mom-config.gradle verify"
+echo "Verifying from configuration in /vagrant/gustavus/configs/vm-mom-config.gradle verify"
 
 
 cd /vagrant/hmt-mom
@@ -15,4 +15,4 @@ gradle clean
 
 echo Beginning verification for folio $1
 
-gradle -Pfolio=$1 -Pconf=${DIR}configs/vm-mom-config.gradle verify
+gradle -Pfolio=$1 -Pconf=/vagrant/gustavus/configs/vm-mom-config.gradle verify
