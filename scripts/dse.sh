4cfdff2257b791b4a7dbe889610f7f4ac2596a19
Stephanie Lindeborg <smlind13@g.holycross.edu>
2014-10-17 18:04:01 -0400

    updated configs and dse script so that the dse test now works in this repo

--- scripts/dse.sh
+++ scripts/dse.sh
@@ -1,18 +1,23 @@
 
-if [ "$#" -ne 1 ]; then
+if [ $# -eq 2 ]; then
+    CONF=/vagrant/gustavus/${2}/configs/vm-mom-config.gradle
+elif [ $# -eq 1 ]; then
+    CONF=/vagrant/gustavus/configs/vm-mom-config.gradle
+else 
     echo "Usage: sh dse.sh URN"
     exit
 fi
 
 
-echo "Verifying from configuration in ${DIR}configs/vm-mom-config.gradle dse"
-
+echo Verifying from configuration in ${CONF}
 
 cd /vagrant/hmt-mom
 
+echo Using MOM clone in `pwd`
 echo "Cleaning previous HMT MOM results..."
 gradle clean
 
-echo Beginning verification for folio $1
+echo Beginning verification for folio $1 using CONF in $CONF
 
-gradle -Pfolio=$1 -Pconf=${DIR}configs/vm-mom-config.gradle dse
+gradle -Pfolio=$1 -Pconf=${CONF} dse
+
