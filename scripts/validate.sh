8ffde48a20022622eecccd9f635228e66b5a1cc9
StephanieL <smlind13@g.holycross.edu>
2016-04-20 14:08:12 -0400

    updated validation from 2 year old files

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -15,4 +15,4 @@ gradle clean
 
 echo Beginning verification for folio $1
 
-gradle -Pfolio=$1 -Pconf=/vagrant/gustavus/configs/vm-mom-config.gradle dse
+gradle -Pfolio=$1 -Pconf=/vagrant/gustavus/configs/vm-mom-config.gradle validate
